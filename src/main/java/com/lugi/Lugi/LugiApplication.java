68dd5818dd027d5bb15acee99bea8ace0134723b
Vitor12Lima <vitorlimarodrigues10@gmail.com>
2018-10-23 13:46:32 -0300

    Configurando o projeto inicial

---
+++ src/main/java/com/lugi/Lugi/LugiApplication.java
@@ -0,0 +1,12 @@
+package com.lugi.Lugi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LugiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LugiApplication.class, args);
+	}
+}
