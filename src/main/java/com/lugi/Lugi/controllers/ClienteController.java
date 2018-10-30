80856e636ca9b3a5a603c4c1bc695b3ba3f2512c
Luciano Melquiades <lucianomelquiades6@gmail.com>
2018-10-30 16:23:54 -0300

    inseriondo metódo Post na classe ClienteController

--- src/main/java/com/lugi/Lugi/controllers/ClienteController.java
+++ src/main/java/com/lugi/Lugi/controllers/ClienteController.java
@@ -1,10 +1,15 @@
 package com.lugi.Lugi.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.lugi.Lugi.model.Cliente;
 import com.lugi.Lugi.repositories.ClienteRepository;
@@ -16,15 +21,15 @@ public class ClienteController {
 	@Autowired
 	ClienteRepository clienteRepository;
 	
-	
-	
     @GetMapping("")
     public Page<Cliente> getCliente(Pageable pageable){
 		return  clienteRepository.findAll(pageable);
 	}
 	
-	
 	@PostMapping("")
+	public Cliente createCliente(@Valid @RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
 	
 	@PutMapping("")
 	
