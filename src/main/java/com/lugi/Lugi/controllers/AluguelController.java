81414d80f1582fe3054e1c88c6134412c2a0dbd9
Luciano Melquiades <lucianomelquiades6@gmail.com>
2018-10-30 14:43:23 -0300

    Criando metódo get na classe AluguelController

--- src/main/java/com/lugi/Lugi/controllers/AluguelController.java
+++ src/main/java/com/lugi/Lugi/controllers/AluguelController.java
@@ -1,8 +1,12 @@
 package com.lugi.Lugi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lugi.Lugi.model.Aluguel;
 import com.lugi.Lugi.repositories.AluguelRepository;
 
 @RestController
@@ -10,6 +14,15 @@ public class AluguelController {
 	
 	@Autowired
 	private AluguelRepository aluguelRepository;
+	
+	
+	@GetMapping("/aluguel")
+	public Page<Aluguel> getAluguel(Pageable pageable){
+		return AluguelRepository.findAll(pageable);
+	}
+	
+	
+	
 	
 	
 
