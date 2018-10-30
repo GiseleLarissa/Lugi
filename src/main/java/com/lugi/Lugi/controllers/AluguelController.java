49c1200642ec31fe56e378f0151f9e04fa6582b9
Gisele Larissa <giselealves016@gmail.com>
2018-10-30 15:34:31 -0300

    adicionando metodo Put na classe AluguelController

--- src/main/java/com/lugi/Lugi/controllers/AluguelController.java
+++ src/main/java/com/lugi/Lugi/controllers/AluguelController.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lugi.Lugi.exception.ResourceNotFoundException;
 import com.lugi.Lugi.model.Aluguel;
 import com.lugi.Lugi.repositories.AluguelRepository;
 
@@ -29,4 +32,21 @@ public class AluguelController {
 	public Aluguel createAluguel(@Valid @RequestBody Aluguel aluguel) {
 		return aluguelRepository.save(aluguel);
 	}	
+	
+	@PutMapping("/aluguel / {aluguelId}")
+	public Aluguel updateAluguel (@PathVariable Long aluguelId, 
+			
+			                                    @Valid @RequestBody Aluguel aluguelRequest) {
+		
+		return aluguelRepository.findById(aluguelId)
+				.map(aluguel -> {
+					aluguel.setDataHoradoAluguel(aluguelRequest.getDataHoradoAluguel());
+					aluguel.setQuilometragePercorrida(aluguelRequest.getQuilometragePercorrida());
+					aluguel.setValor(aluguelRequest.getValor());
+					aluguel.setTipoPagamento(aluguelRequest.getTipoPagamento());
+					return aluguelRepository.save(aluguel);
+		}).orElseThrow(() -> new ResourceNotFoundException("FamilyMember not found: " + aluguelId));
+	}
+				
 }
+
