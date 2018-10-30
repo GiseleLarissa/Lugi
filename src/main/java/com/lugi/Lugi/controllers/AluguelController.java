3e8355111b0573c74722193195be235d3a7bf90c
Gisele Larissa <giselealves016@gmail.com>
2018-10-30 15:15:56 -0300

    adicionando metodo post na classe controller

--- src/main/java/com/lugi/Lugi/controllers/AluguelController.java
+++ src/main/java/com/lugi/Lugi/controllers/AluguelController.java
@@ -1,9 +1,13 @@
 package com.lugi.Lugi.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lugi.Lugi.model.Aluguel;
@@ -18,12 +22,11 @@ public class AluguelController {
 	
 	@GetMapping("/aluguel")
 	public Page<Aluguel> getAluguel(Pageable pageable){
-		return AluguelRepository.findAll(pageable);
+		return aluguelRepository.findAll(pageable);
 	}
 	
-	
-	
-	
-	
-
+	@PostMapping("/aluguel")
+	public Aluguel createAluguel(@Valid @RequestBody Aluguel aluguel) {
+		return aluguelRepository.save(aluguel);
+	}	
 }
