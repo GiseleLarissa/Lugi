e0243bffd1b5fa70b1c81647599e88dd5f105bc2
Luciano Melquiades <lucianomelquiades6@gmail.com>
2018-11-01 13:57:27 -0300

    Metodo get na classe Locadora controller

--- src/main/java/com/lugi/Lugi/controllers/LocadoraController.java
+++ src/main/java/com/lugi/Lugi/controllers/LocadoraController.java
@@ -1,8 +1,12 @@
 package com.lugi.Lugi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lugi.Lugi.model.Locadora;
 import com.lugi.Lugi.repositories.LocadoraRepository;
 
 @RestController
@@ -13,6 +17,12 @@ public class LocadoraController {
 	
 	
     @GetMapping("/carro")
+    public Page<Locadora> getLocadora(Pageable pageable){
+    	return locadoraRepository.findAll(pageable);
+    }
+    
+    
+    
 	
 	@PostMapping("/carro")
 	
