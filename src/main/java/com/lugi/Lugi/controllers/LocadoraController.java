a5c6cd63772ff2dabcb3cd39dcfa54141af20e4f
Luciano Melquiades <lucianomelquiades6@gmail.com>
2018-11-01 14:07:53 -0300

    Metodo put na classe Locadora controller

--- src/main/java/com/lugi/Lugi/controllers/LocadoraController.java
+++ src/main/java/com/lugi/Lugi/controllers/LocadoraController.java
@@ -2,14 +2,19 @@ package com.lugi.Lugi.controllers;
 
 import javax.validation.Valid;
 
+import org.hibernate.sql.Update;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lugi.Lugi.exception.ResourceNotFoundException;
+import com.lugi.Lugi.model.Carro;
 import com.lugi.Lugi.model.Locadora;
 import com.lugi.Lugi.repositories.LocadoraRepository;
 
@@ -30,9 +35,24 @@ public class LocadoraController {
 		return locadoraRepository.save(locadora);
 	}
 	
-	
 	@PutMapping("/carro")
-	
+	public Locadora updateLocadora(@PathVariable Long locadoraId, 
+			
+			 @Valid @RequestBody Locadora locadoraRequest) {
+
+		return locadoraRepository.findById(locadoraId)
+		.map( locadora -> {
+		locadora.setCnpj(locadoraRequest.getCnpj());
+		locadora.setContato(locadoraRequest.getContato());
+		locadora.setEndereco(locadoraRequest.getEndereco());
+		locadora.setCarros(locadoraRequest.getCarros());
+		locadora.setClientes(locadoraRequest.getClientes());
+		return locadoraRepository.save(locadora);
+		}).orElseThrow(() -> new ResourceNotFoundException("FamilyMember not found: " + locadoraId));
+		}
+			
+		
+			
 	
 	@DeleteMapping("/carro")
 
