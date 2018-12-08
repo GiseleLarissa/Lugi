464219b9cf89ef188d1e4aad6f2baaf3c161ab89
Luciano Melquiades <lucianomelquiades6@gmail.com>
2018-12-08 16:39:00 -0300

    testes

--- src/main/java/com/lugi/Lugi/servico/ClienteValidacao.java
+++ src/main/java/com/lugi/Lugi/servico/ClienteValidacao.java
@@ -1,5 +1,56 @@
 package com.lugi.Lugi.servico;
 
-public class ClienteValidacao {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class ClienteValidacao {
+	
+	private static final String EMAIL_PATTERN = 
+	        "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+	        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	
+	private static final String NOME_PATTERN = "^[A-Z][a-z]* [ [A-Z][a-z]]*";
+	
+	private static final String CELULAR_PATTERN = "[0-9]{1,2}\\s[0-9]{4,5}[-][0-9]{4,5}";
+	
+	private static  Pattern pattern;
+		
+	private static Matcher matcher;
+	
+
+	public boolean validarCliente(String nome, String email,  double cpf, int celular) {
+		
+		if(validarEmail(email) && validarNome(nome) && validarCelular(celular)) {
+			return true;
+		}
+		return false;
+	}
+
+	
+	//public boolean validarCpf(double cpf) {}
+	
+	public  boolean validarEmail(String email){
+		
+	    pattern = Pattern.compile(EMAIL_PATTERN, Pattern.CASE_INSENSITIVE);		
+		matcher= pattern.matcher(email);
+	    return matcher.matches();
+	 
+	}
+	
+	public boolean validarNome(String nome) {
+		
+		pattern = Pattern.compile(NOME_PATTERN, Pattern.CASE_INSENSITIVE);
+		matcher = pattern.matcher(nome);
+		return matcher.matches();
+	
+	}
+	
+	public boolean validarCelular(int celular) {
+		pattern = Pattern.compile(CELULAR_PATTERN, Pattern.CASE_INSENSITIVE);
+		matcher = pattern.matcher(celular);
+		return matcher.matches();
+		
+	}
 }
+
+
--- src/test/java/com/lugi/Lugi/servico/ClienteValidacaoTest.java
+++ src/test/java/com/lugi/Lugi/servico/ClienteValidacaoTest.java
@@ -1,5 +1,184 @@
 package com.lugi.Lugi.servico;
 
-public class ClienteValidacaoTest {
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
+public class ClienteValidacaoTest {
+	
+	
+	//Testes para nome
+	@Test
+	public void naoPodeValidarNomeComNumeros() {
+		ClienteValidacao cliente = new ClienteValidacao();
+
+		String nome = "Luciano Melquiades 393";
+		String email = "luciano6@gmail.com";
+		double cpf = 1937483728; 
+		int celular = 340493343;
+		//LocalDate dataDeNascimento ( 2000,10,6);
+		
+		
+		boolean ehUsuarioValido = cliente.validarCliente(nome, email, cpf, celular);
+		
+		assertEquals(false, ehUsuarioValido);
+	}
+	@Test
+	public void naoPodeValidarNomeESobrenomeEscritosSemEspaco() {
+		ClienteValidacao cliente = new ClienteValidacao();
+
+		String nome = "LucianoMelquiades";
+		String email = "luciano6@gmail.com";
+		double cpf = 1937483728; 
+		int celular = 340493343;
+		//LocalDate dataDeNascimento ( 2000,10,6);
+		
+		
+		boolean ehUsuarioValido = cliente.validarCliente(nome, email, cpf, celular);
+		
+		assertEquals(false, ehUsuarioValido);
+	}
+	@Test
+	public void permitirValidarComTudoCorreto() {
+		ClienteValidacao cliente = new ClienteValidacao();
+
+		String nome = "Luciano Melquiades";
+		String email = "luciano6@gmail.com";
+		double cpf = 1937483728; 
+		int celular = 340493343;
+		//LocalDate dataDeNascimento ( 2000,10,6);
+		
+		
+		boolean ehUsuarioValido = cliente.validarCliente(nome, email, cpf, celular);
+		
+		assertEquals(true, ehUsuarioValido);
+	}
+	
+	
+	//Testes para cpf 
+	@Test
+	public void permitirValidarComCpfcomPonto() {
+		ClienteValidacao cliente = new ClienteValidacao();
+
+		String nome = "Luciano Melquiades";
+		String email = "luciano6@gmail.com";
+		double cpf = 193748372-58; 
+		int celular = 340493343;
+		//LocalDate dataDeNascimento ( 2000,10,6);
+		
+		
+		boolean ehUsuarioValido = cliente. validarCliente(nome, email, cpf, celular) ;
+		
+		assertEquals(true, ehUsuarioValido);
+	}
+	@Test
+	public void permitirValidarComCpfsemPonto() {
+		ClienteValidacao cliente = new ClienteValidacao();
+
+		String nome = "Luciano Melquiades";
+		String email = "luciano6@gmail.com";
+		double cpf = 19837258; 
+		int celular = 340493343;
+		//LocalDate dataDeNascimento ( 2000,10,6);
+		
+		
+		boolean ehUsuarioValido = cliente. validarCliente(nome, email, cpf, celular) ;
+		
+		assertEquals(true, ehUsuarioValido);
+	}
+	
+	
+	
+	//Testes para email 
+	
+	@Test
+	public void nãoPodesemArroba() {
+		ClienteValidacao cliente = new ClienteValidacao();
+
+		String nome = "Luciano Melquiades";
+		String email = "luciano6gmail.com";
+		double cpf = 1937483728; 
+		int celular = 340493343;
+		//LocalDate dataDeNascimento ( 2000,10,6);
+		
+		
+		boolean ehUsuarioValido = cliente.validarCliente(nome, email, cpf, celular);
+		
+		assertEquals(false, ehUsuarioValido);
+	}
+
+	@Test
+	public void nãoPodesemPonto() {
+		ClienteValidacao cliente = new ClienteValidacao();
+
+		String nome = "Luciano Melquiades";
+		String email = "luciano6gmailcom";
+		double cpf = 1937483728; 
+		int celular = 340493343;
+		//LocalDate dataDeNascimento ( 2000,10,6);
+		
+		
+		boolean ehUsuarioValido = cliente.validarCliente(nome, email, cpf, celular);
+		
+		assertEquals(false, ehUsuarioValido);
+	}
+	
+	@Test
+	public void nãoPodeComCaracteresEspeciais() {
+		ClienteValidacao cliente = new ClienteValidacao();
+
+		String nome = "Luciano Melquiades";
+		String email = "lu@c%ian#o6@gmail.com";
+		double cpf = 1937483728; 
+		int celular = 340493343;
+		//LocalDate dataDeNascimento ( 2000,10,6);
+		
+		
+		boolean ehUsuarioValido = cliente.validarCliente(nome, email, cpf, celular);
+		
+		assertEquals(false, ehUsuarioValido);
+	}
+	
+	
+	
+	
+	//Testes para Celular 
+	
+	@Test
+	public void permitirNoveNumeros() {
+		ClienteValidacao cliente = new ClienteValidacao();
+
+		String nome = "Luciano Melquiades";
+		String email = "luciano6@gmail.com";
+		double cpf = 1937483728; 
+		int celular = 987085244;
+		//LocalDate dataDeNascimento ( 2000,10,6);
+		
+		
+		boolean ehUsuarioValido = cliente.validarCliente(nome, email, cpf, celular);
+		
+		assertEquals(true, ehUsuarioValido);
+	}
+	
+
+	@Test
+	public void nãoPermitirMaisDeNoveNumeros() {
+		ClienteValidacao cliente = new ClienteValidacao();
+
+		String nome = "Luciano Melquiades";
+		String email = "luciano6@gmail.com";
+		double cpf = 1937483728; 
+		int celular = 987879049;
+		//LocalDate dataDeNascimento ( 2000,10,6);
+		
+		
+		boolean ehUsuarioValido = cliente.validarCliente(nome, email, cpf, celular);
+		
+		assertEquals(false, ehUsuarioValido);
+	}
+	
+	
+	
+	//Testes para Data de nascimento 
+	
+	
 }
